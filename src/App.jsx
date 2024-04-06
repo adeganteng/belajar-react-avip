06f3510614653373d8523b969822a3fdc12bd921
adeganteng <wicaksonoade21@gmail.com>
2024-04-06 11:26:08 +0700

    1. Set up react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-slate-200 min-h-screen items-center">
+      <h1 className="text-center text-3xl text-slate-950 font-bold">Ade Wicaksono</h1>
+    </div>
+  );
+}
+
+export default App;
