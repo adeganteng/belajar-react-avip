ad538d17737d34d28e344ceff342795cb42f9459
adeganteng <wicaksonoade21@gmail.com>
2024-04-08 10:48:59 +0700

    8. Rendering list pada product

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -2,34 +2,40 @@ import Button from "../Elements/Button/Button";
 
 const CardProduct = ({ children }) => {
   return (
-    <div className="card md:w-80 bg-slate-700 shadow-md w-full">{children}</div>
+    <div className="card md:w-80 bg-slate-700 shadow-md w-full h-96 flex flex-col justify-between pb-5">{children}</div>
   );
 };
 
 const HeaderCard = ({image}) => {
   return (
-    <figure>
-      <img src={image} alt="car!" />
+    <figure className="min-h-40">
+      <img src={image} alt="car!" className="w-full h-40"/>
     </figure>
   );
 };
 
-const BodyCard = ({title, description, price}) => {
+const BodyCard = ({title, description}) => {
   return (
     <div className="card-body">
           <h2 className="card-title text-slate-100 font-bold">{ title }</h2>
-      <p className="text-slate-300 font-semibold">
+      <p className="text-slate-300 font-semibold text-clip">
         {description}
       </p>
-      <div className="card-actions justify-between mt-3 items-center">
-              <p className="text-slate-100 font-bold text-sm">{ price }</p>
-        <Button variant={"bg-blue-500 text-white"} text={"Add to cart"} />
-      </div>
     </div>
   );
 };
 
+const FooterCard = ({price}) => {
+    return (
+      <div className="card-actions justify-between items-center px-5">
+        <p className="text-slate-100 font-bold text-sm">{price}</p>
+        <Button variant={"bg-blue-500 text-white"} text={"Add to cart"} />
+      </div>
+    );
+}
+
 CardProduct.HeaderCard = HeaderCard;
 CardProduct.BodyCard = BodyCard;
+CardProduct.FooterCard = FooterCard;
 
 export default CardProduct;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -5,51 +5,70 @@ const ProductsPage = () => {
   return (
     <div className="bg-slate-200 min-h-screen flex flex-col justify-center pt-24 px-10 md:px-0">
       <NavbarLayouts />
-          <div className="flex flex-row flex-wrap justify-center gap-3">
-              
-        <CardProduct>
-          <CardProduct.HeaderCard image={"/images/product-1.jpg"} />
-          <CardProduct.BodyCard
-            title={"Sepatu Adidas"}
-            description={"Sepatu model Kekinian buat tambah keren"}
-            price={"Rp 1.000.000;"}
-          />
-        </CardProduct>
-        <CardProduct>
-          <CardProduct.HeaderCard image={"/images/product-1.jpg"} />
-          <CardProduct.BodyCard
-            title={"Sepatu Adidas"}
-            description={"Sepatu model Kekinian buat tambah keren"}
-            price={"Rp 1.000.000;"}
-          />
-        </CardProduct>
-        <CardProduct>
-          <CardProduct.HeaderCard image={"/images/product-1.jpg"} />
-          <CardProduct.BodyCard
-            title={"Sepatu Adidas"}
-            description={"Sepatu model Kekinian buat tambah keren"}
-            price={"Rp 1.000.000;"}
-          />
-        </CardProduct>
-        <CardProduct>
-          <CardProduct.HeaderCard image={"/images/product-1.jpg"} />
-          <CardProduct.BodyCard
-            title={"Sepatu Adidas"}
-            description={"Sepatu model Kekinian buat tambah keren"}
-            price={"Rp 1.000.000;"}
-          />
-        </CardProduct>
-        <CardProduct>
-          <CardProduct.HeaderCard image={"/images/product-1.jpg"} />
-          <CardProduct.BodyCard
-            title={"Sepatu Adidas"}
-            description={"Sepatu model Kekinian buat tambah keren"}
-            price={"Rp 1.000.000;"}
-          />
-        </CardProduct>
+      <div className="flex flex-row flex-wrap justify-center items-start gap-3">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.HeaderCard image={product.image} />
+            <CardProduct.BodyCard
+              title={product.title}
+              description={product.description}
+            />
+            <CardProduct.FooterCard price={product.price} />
+          </CardProduct>
+        ))}
       </div>
     </div>
   );
 };
+
+const products = [
+  {
+    id: 1,
+    image: "/images/product-1.jpg",
+    title: "Adidas Y-3",
+    description:
+      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
+    price: "Rp 1.000.000;",
+  },
+  {
+    id: 2,
+    image: "/images/product-1.jpg",
+    title: "Adidas Y-3",
+    description:
+      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
+    price: "Rp 1.000.000;",
+  },
+  {
+    id: 3,
+    image: "/images/product-1.jpg",
+    title: "Adidas Y-3",
+    description:
+      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
+    price: "Rp 1.000.000;",
+  },
+  {
+    id: 4,
+    image: "/images/product-1.jpg",
+    title: "Adidas Y-3",
+    description:
+      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
+    price: "Rp 1.000.000;",
+  },
+  {
+    id: 5,
+    image: "/images/product-1.jpg",
+    title: "Adidas Y-3",
+    description:
+      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipake Real Madrid pada jreseynya",
+    price: "Rp 1.000.000;",
+  },
+  {
+    id: 6,
+    image: "/images/product-1.jpg",
+    title: "Adidas Y-3",
+    description: "Adidas Y-3 adalah produk adidas yang sangat populer yang dipake tim-tim besar",
+    price: "Rp 1.000.000;",
+  },
+];
 
 export default ProductsPage;
