9394b8a51315b2cca7d33dd5d71b622dbb04740c
adeganteng <wicaksonoade21@gmail.com>
2024-04-12 14:12:12 +0700

    14. Belajar Fetch API di REACT menggunakan axios, pengimplementasian pada products

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -2,37 +2,49 @@ import Button from "../Elements/Button/Button";
 
 const CardProduct = ({ children }) => {
   return (
-    <div className="max-w-xs card md:w-80 bg-slate-700 shadow-md h-96 flex flex-col justify-between">{children}</div>
+    <div className="max-w-xs card md:w-80 bg-slate-700 shadow-md h-96 flex flex-col justify-between">
+      {children}
+    </div>
   );
 };
 
-const HeaderCard = ({image}) => {
+const HeaderCard = ({ image }) => {
   return (
-    <figure>
-      <img src={image} alt="car!"/>
+    <figure className="w-full h-2/5 object-cover">
+      <img src={image} alt="car!" />
     </figure>
   );
 };
 
-const BodyCard = ({title, description}) => {
+const BodyCard = ({ title, description }) => {
   return (
     <div className="card-body">
-          <h2 className="card-title text-slate-100 font-bold">{ title }</h2>
-      <p className="text-slate-300 font-semibold text-clip">
-        {description}
+      <h2 className="card-title text-slate-100 font-bold ">
+        {title.substring(0, 20)}...
+      </h2>
+      <p className="text-slate-300 font-semibold text-clip ">
+        {description.substring(0, 75)}...
       </p>
     </div>
   );
 };
 
-const FooterCard = ({price, handleAddToCart, id}) => {
-    return (
-      <div className="card-actions justify-between items-center px-5 pb-3">
-        <p className="text-slate-100 font-bold text-sm">Rp {price.toLocaleString('id-ID', { styles: 'currency', currency: 'IDR' })};</p>
-        <Button variant={"bg-blue-500 text-white"} text={"Add to cart"} onClick={()=>handleAddToCart(id)}/>
-      </div>
-    );
-}
+const FooterCard = ({ price, handleAddToCart, id }) => {
+  return (
+    <div className="card-actions justify-between items-center px-5 pb-3">
+      <p className="text-slate-100 font-bold text-sm">
+        $
+        {price.toLocaleString("id-ID", { styles: "currency", currency: "USD" })}
+        ;
+      </p>
+      <Button
+        variant={"bg-blue-500 text-white"}
+        text={"Add to cart"}
+        onClick={() => handleAddToCart(id)}
+      />
+    </div>
+  );
+};
 
 CardProduct.HeaderCard = HeaderCard;
 CardProduct.BodyCard = BodyCard;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import NavbarLayouts from "../components/Layouts/NavbarLayouts";
+import { getProduct } from "../services/product.service";
 
 const ProductsPage = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
   }, []);
 
   useEffect(() => {
-    if (cart.length > 0) {
+    if (products.length >0 && cart.length > 0) {
       const sum = cart.reduce((acc, item) => {
         const product = products.find((product) => product.id === item.id);
         return acc + product.price * item.qty;
@@ -19,7 +21,7 @@ const ProductsPage = () => {
       setTotalPrice(sum);
       localStorage.setItem("cart", JSON.stringify(cart));
     }
-  }, [cart]);
+  }, [cart, products]);
 
   const handleAddToCart = (id) => {
     if (cart.find((item) => item.id === id)) {
@@ -33,6 +35,11 @@ const ProductsPage = () => {
     }
   };
 
+  useEffect(() => {
+    getProduct((data) => {
+      setProducts(data)
+    });
+  }, []);
   // useRef
   const totalPriceRef = useRef(null);
   useEffect(() => {
@@ -46,20 +53,21 @@ const ProductsPage = () => {
     <div className="bg-slate-200 min-h-screen flex justify-center pt-24 px-10 md:px-0">
       <NavbarLayouts />
       <div className="flex flex-row flex-wrap justify-center items-start gap-3 w-1/2">
-        {products.map((product) => (
-          <CardProduct key={product.id}>
-            <CardProduct.HeaderCard image={product.image} />
-            <CardProduct.BodyCard
-              title={product.title}
-              description={product.description}
-            />
-            <CardProduct.FooterCard
-              price={product.price}
-              handleAddToCart={handleAddToCart}
-              id={product.id}
-            />
-          </CardProduct>
-        ))}
+        {products.length > 0 &&
+          products.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.HeaderCard image={product.image} />
+              <CardProduct.BodyCard
+                title={product.title}
+                description={product.description}
+              />
+              <CardProduct.FooterCard
+                price={product.price}
+                handleAddToCart={handleAddToCart}
+                id={product.id}
+              />
+            </CardProduct>
+          ))}
       </div>
       <div className="w-1/2 bg-slate-400 p-3">
         <h1 className="text-blue-700 font-bold text-3xl">Cart</h1>
@@ -76,7 +84,7 @@ const ProductsPage = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {cart.map((item) => {
+            {products.length > 0 && cart.map((item) => {
               const product = products.find(
                 (product) => product.id === item.id
               );
@@ -85,18 +93,18 @@ const ProductsPage = () => {
                 <tr key={item.id}>
                   <td className="text-slate-950 text-sm">{product.title}</td>
                   <td className="text-slate-950 text-sm">
-                    Rp{" "}
+                    $
                     {product.price.toLocaleString("id-ID", {
                       styles: "currency",
-                      currency: "IDR",
+                      currency: "USD",
                     })}
                   </td>
                   <td className="text-slate-950 text-sm">{item.qty}</td>
                   <td className="text-slate-950 text-sm">
-                    Rp{" "}
+                    $
                     {(item.qty * product.price).toLocaleString("id-ID", {
                       styles: "currency",
-                      currency: "IDR",
+                      currency: "USD",
                     })}
                   </td>
                 </tr>
@@ -108,10 +116,10 @@ const ProductsPage = () => {
               </td>
               <td>
                 <b className="text-slate-950 text-sm">
-                  Rp{" "}
+                  $
                   {totalPrice.toLocaleString("id-ID", {
                     styles: "currency",
-                    currency: "IDR",
+                    currency: "USD",
                   })}
                   ;
                 </b>
@@ -123,56 +131,5 @@ const ProductsPage = () => {
     </div>
   );
 };
-
-const products = [
-  {
-    id: 1,
-    image: "/images/product-1.jpg",
-    title: "Adidas Y-3 1",
-    description:
-      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: 1000000,
-  },
-  {
-    id: 2,
-    image: "/images/product-1.jpg",
-    title: "Adidas Y-3 2",
-    description:
-      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: 1000000,
-  },
-  {
-    id: 3,
-    image: "/images/product-1.jpg",
-    title: "Adidas Y-3 3",
-    description:
-      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: 2000000,
-  },
-  {
-    id: 4,
-    image: "/images/product-1.jpg",
-    title: "Adidas Y-3 4",
-    description:
-      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: 2000000,
-  },
-  {
-    id: 5,
-    image: "/images/product-1.jpg",
-    title: "Adidas Y-3 5",
-    description:
-      "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipake Real Madrid pada jreseynya",
-    price: 1500000,
-  },
-  {
-    id: 6,
-    image: "/images/product-1.jpg",
-    title: "Adidas Y-3",
-    description:
-      "Adidas Y-3 adalah produk adidas yang sangat populer yang dipake tim-tim besar",
-    price: 4000000,
-  },
-];
 
 export default ProductsPage;
