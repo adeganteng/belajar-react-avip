b1f241c9adb583eb6f3bcfe7ad932cf1e3d9e630
adeganteng <wicaksonoade21@gmail.com>
2024-04-12 08:08:01 +0700

    11. Belajar sedikit yaitu cara menggunakan useState

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button/Button";
 
 const CardProduct = ({ children }) => {
   return (
-    <div className="card md:w-80 bg-slate-700 shadow-md w-full h-96 flex flex-col justify-between">{children}</div>
+    <div className="max-w-xs card md:w-80 bg-slate-700 shadow-md h-96 flex flex-col justify-between">{children}</div>
   );
 };
 
@@ -25,11 +25,11 @@ const BodyCard = ({title, description}) => {
   );
 };
 
-const FooterCard = ({price}) => {
+const FooterCard = ({price, handleAddToCart, id}) => {
     return (
       <div className="card-actions justify-between items-center px-5 pb-3">
-        <p className="text-slate-100 font-bold text-sm">{price}</p>
-        <Button variant={"bg-blue-500 text-white"} text={"Add to cart"} />
+        <p className="text-slate-100 font-bold text-sm">Rp {price.toLocaleString('id-ID', { styles: 'currency', currency: 'IDR' })};</p>
+        <Button variant={"bg-blue-500 text-white"} text={"Add to cart"} onClick={()=>handleAddToCart(id)}/>
       </div>
     );
 }
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,30 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import NavbarLayouts from "../components/Layouts/NavbarLayouts";
 
-// Belajar sedikit tentang state
-class Counter extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      count : 0,
-    }
-  }
-  render() {
-    return (
-      <div className="flex gap-3 items-center">
-        <h1 className="text-3xl font-bold text-slate-950">{ this.state.count }</h1>
-        <button className="bg-black text-white p-3" onClick={() => this.setState({count : this.state.count+1}) }>+</button>
-      </div>
-    )
-  }
-}
-
 const ProductsPage = () => {
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    },
+  ]);
+
+  const handleAddToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
+  };
   return (
-    <div className="bg-slate-200 min-h-screen flex flex-col justify-center pt-24 px-10 md:px-0">
+    <div className="bg-slate-200 min-h-screen flex justify-center pt-24 px-10 md:px-0">
       <NavbarLayouts />
-      <div className="flex flex-row flex-wrap justify-center items-start gap-3">
+      <div className="flex flex-row flex-wrap justify-center items-start gap-3 w-1/2">
         {products.map((product) => (
           <CardProduct key={product.id}>
             <CardProduct.HeaderCard image={product.image} />
@@ -32,12 +32,57 @@ const ProductsPage = () => {
               title={product.title}
               description={product.description}
             />
-            <CardProduct.FooterCard price={product.price} />
+            <CardProduct.FooterCard
+              price={product.price}
+              handleAddToCart={handleAddToCart}
+              id={product.id}
+            />
           </CardProduct>
         ))}
       </div>
-      <div className="flex justify-center ">
-        <Counter />
+      <div className="w-1/2 bg-slate-400 p-3">
+        <h1 className="text-blue-700 font-bold text-3xl">Cart</h1>
+
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th className="text-slate-950 text-sm">Name</th>
+              <th className="text-slate-950 text-sm">Price</th>
+              <th className="text-slate-950 text-sm">Qty</th>
+              <th className="text-slate-950 text-sm">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item) => {
+              const product = products.find(
+                (product) => product.id === item.id
+              );
+              // if (!product) return null; // Add this line to handle undefined product
+              return (
+                <tr key={item.id}>
+                  <td className="text-slate-950 text-sm">{product.title}</td>
+                  <td className="text-slate-950 text-sm">
+                    Rp{" "}
+                    {product.price.toLocaleString("id-ID", {
+                      styles: "currency",
+                      currency: "IDR",
+                    })}
+                  </td>
+                  <td className="text-slate-950 text-sm">{item.qty}</td>
+                  <td className="text-slate-950 text-sm">
+                    Rp{" "}
+                    {(item.qty * product.price).toLocaleString("id-ID", {
+                      styles: "currency",
+                      currency: "IDR",
+                    })}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
@@ -47,49 +92,50 @@ const products = [
   {
     id: 1,
     image: "/images/product-1.jpg",
-    title: "Adidas Y-3",
+    title: "Adidas Y-3 1",
     description:
       "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: "Rp 1.000.000;",
+    price: 1000000,
   },
   {
     id: 2,
     image: "/images/product-1.jpg",
-    title: "Adidas Y-3",
+    title: "Adidas Y-3 2",
     description:
       "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: "Rp 1.000.000;",
+    price: 1000000,
   },
   {
     id: 3,
     image: "/images/product-1.jpg",
-    title: "Adidas Y-3",
+    title: "Adidas Y-3 3",
     description:
       "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: "Rp 1.000.000;",
+    price: 2000000,
   },
   {
     id: 4,
     image: "/images/product-1.jpg",
-    title: "Adidas Y-3",
+    title: "Adidas Y-3 4",
     description:
       "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipak Real Madrid",
-    price: "Rp 1.000.000;",
+    price: 2000000,
   },
   {
     id: 5,
     image: "/images/product-1.jpg",
-    title: "Adidas Y-3",
+    title: "Adidas Y-3 5",
     description:
       "Adidas Y-3 adalah produk adidas yang sangat populer dan biasanya dipake Real Madrid pada jreseynya",
-    price: "Rp 1.000.000;",
+    price: 1500000,
   },
   {
     id: 6,
     image: "/images/product-1.jpg",
     title: "Adidas Y-3",
-    description: "Adidas Y-3 adalah produk adidas yang sangat populer yang dipake tim-tim besar",
-    price: "Rp 1.000.000;",
+    description:
+      "Adidas Y-3 adalah produk adidas yang sangat populer yang dipake tim-tim besar",
+    price: 4000000,
   },
 ];
 
